827af9ae341a4bc71202d3a4a43b3d8a4c6f01ca
Shreyas280 <62773720+Shreyas280@users.noreply.github.com>
2021-01-25 18:36:12 -0600

    Create population.c

---
+++ population.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+        int start;
+    do
+    {
+        start = get_int("start size: ");
+    }
+    while (start < 9);
+
+    int end;
+    do
+    {
+        end = get_int("end size: ");
+    }
+    while (end < start);
+
+        int years = 0;
+        
+        while (start < end)
+
+    {
+    start = start + (start / 3) - (start / 4);
+    years++;
+    }
+
+    {
+    printf("years: %i\n", years);
+    }
+}
